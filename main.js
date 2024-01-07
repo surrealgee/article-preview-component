f3920a631cd2b6cb9b935490ed2bb9835a27adf6
Surreal Gee <94802496+surrealgee@users.noreply.github.com>
2024-01-07 18:43:56 -0400

    Build active state for desktop

--- main.js
+++ main.js
@@ -7,13 +7,51 @@ const shareBtnActive = document.querySelector('#share-btn-active');
 // Event Listeners
 
 shareBtn.addEventListener('click', () => {
-    shareBox.style.opacity = 1;
-    shareBox.style.bottom = 0;
-    shareBox.style.padding = "1.40rem 2rem";
+    changeBtnStyle();
+
+    if (shareBox.style.opacity == 0) {
+        showModal()
+    } else {
+        hideModal()
+
+    }
 })
 
 shareBtnActive.addEventListener('click', () => {
+    changeBtnStyle();
     shareBox.style.opacity = 0;
     shareBox.style.bottom = "-30px";
     shareBox.style.padding = "0 2rem";
+
+    
 })
+
+// Functions
+
+function changeBtnStyle() {
+    if (shareBox.style.opacity == 0) {
+        shareBtn.style.backgroundColor = "#6E8098";
+        shareBtn.firstElementChild.src = "./images/icon-share-hover.svg"
+    } else {
+        shareBtn.style.backgroundColor = "#ECF2F8";
+        shareBtn.firstElementChild.src = "./images/icon-share.svg"
+
+    }
+}
+
+function showModal() {
+    if (window.innerWidth > 800) {
+        shareBox.style.opacity = 1;
+        shareBox.style.right = "-85px";
+        shareBox.style.bottom = "65px";
+    } else {
+        shareBox.style.opacity = 1;
+        shareBox.style.bottom = 0;
+        shareBox.style.right = 0;
+        shareBox.style.padding = "1.40rem 2rem";
+    }
+}
+
+function hideModal() {
+    shareBox.style.opacity = 0;
+}
